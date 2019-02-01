5e42c9da6be10e8ae255b702557731adb51dbba7
Eric Seidler <eric.seidler@asynchrony.com>
2019-02-01 07:52:11 -0600

    add poem number to page

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ fetch('https://raw.githubusercontent.com/RyanHirsch/potp/master/README.md')
   .then(response => response.text())
   .then(potpText => {
     const justPoemLines = potpText.split(/\n/).slice(2);
-    const numPoems = justPoemLines.length;
-    const randPoem = justPoemLines[getRandomInt(0, numPoems - 1)].split('1. ')[1];
+    const numPoems = justPoemLines.length - 1;  // account for trailing newline
+    const randPoemIndex = getRandomInt(0, numPoems - 1);
+    const randPoemNumber = randPoemIndex + 1;
+    const randPoem = justPoemLines[randPoemIndex].split('1. ')[1];
+    document.getElementById("poem-number").innerHTML = '#' + randPoemNumber;
     document.getElementById("random-poem").innerHTML = randPoem;
   });
 
